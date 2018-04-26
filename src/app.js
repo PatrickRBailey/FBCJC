83b911ec15b3c0596eb91271f2b4c60f849c0711
Patrick Bailey <patrick@ieqtechnology.com>
2018-04-26 12:56:42 -0700

    changes title and adds eslint config

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
+import 'normalize.css/normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
